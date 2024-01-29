ed7aa281a597c9225f7309030d5223253c95f353
RebeccaKelble <156012978+RebeccaKelble@users.noreply.github.com>
2024-01-29 10:30:15 -0800

    Wrote code

--- code/generate_data.R
+++ code/generate_data.R
@@ -12,3 +12,16 @@ noise_level <- 3
 ntimepoints <- 150
 
 # Sample population sizes
+pop_size <- rnorm(n = ntimepoints,
+                  mean = mean_abundance,
+                  sd = noise_level)
+
+# Diagnostic plot
+par(cex = 3)
+plot(pop_size,
+     xlab = "Time",
+     ylab = "Population size")
+
+# Save data
+filename <- "/data/popdata.Rdata"
+save(pop_size, file = filename)
